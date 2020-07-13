02acbaa55cffdd96057b904d4ae0bda5f50691cf
muzing <muzi2001@foxmail.com>
2020-07-13 20:04:27 +0800

    增加了关于对象父子关系的笔记代码

--- 04-QObject.py
+++ 04-QObject.py
@@ -12,7 +12,8 @@ class Window(QWidget):
     def setup_ui(self):
         # self.QObject继承结构测试()
         # self.QObject对象名称和属性的操作()
-        self.QObject案例测试()
+        # self.QObject案例测试()
+        self.QObject对象的父子关系操作()
 
     def QObject继承结构测试(self):
         mros = QObject.mro()
@@ -45,6 +46,42 @@ class Window(QWidget):
         label2.setText("人狠话不多")
         label2.setObjectName("notice")
         label2.setProperty("notice_level", "normal")
+
+    def QObject对象的父子关系操作(self):
+        # 测试API
+        obj0 = QObject()
+        obj1 = QObject()
+        obj2 = QObject()
+        obj3 = QObject()
+        obj4 = QObject()
+        obj5 = QObject()
+        print('obj0', obj0)
+        print('obj1', obj1)
+        print('obj2', obj2)
+        print('obj3', obj3)
+        print('obj4', obj4)
+        print('obj5', obj5)
+
+        obj1.setParent(obj0)
+        obj2.setParent(obj0)
+        obj2.setObjectName('2')
+
+        # label = QLabel()
+        # label.setParent(obj0)
+
+        obj3.setParent(obj1)
+        obj3.setObjectName('3')
+        obj4.setParent(obj2)
+        obj5.setParent(obj2)
+
+        # print(obj4.parent())
+        # print(obj0.children())
+        print(obj0.findChild(QObject))
+        print(obj0.findChild(QObject, '2'))
+        print(obj0.findChild(QObject, '3'))
+        print(obj0.findChild(QObject, '3', Qt.FindDirectChildrenOnly))
+        print(obj0.findChildren(QObject))
+        # print(obj0.findChild(QLabel))  # 会报错
 
 
 if __name__ == '__main__':
