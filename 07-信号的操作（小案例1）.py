728ac3ae1ee2c9e642b7988285ff31e472e99b8c
muzing <muzi2001@foxmail.com>
2020-07-16 10:31:47 +0800

    增加信号的操作的两个小案例

---
+++ 07-信号的操作（小案例1）.py
@@ -0,0 +1,28 @@
+from PyQt5.Qt import *
+import sys
+
+
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("信号的操作案例")
+        self.resize(500, 500)
+        self.move(400, 250)
+        self.setup_ui()
+
+    def setup_ui(self):
+        btn = QPushButton(self)
+        btn.setText("点击我")
+
+        def cao():
+            print('点我干哈？')
+        btn.clicked.connect(cao)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    window = Window()
+    window.show()
+
+    sys.exit(app.exec_())
---
+++ 08-信号的操作（小案例2）.py
@@ -0,0 +1,28 @@
+from PyQt5.Qt import *
+import sys
+# 要求： 每次改变窗口标题，都自动加上muzing前缀，能重复使用
+app = QApplication(sys.argv)
+
+window = QWidget()
+# 连接窗口标题变化的 信号 与 槽
+
+
+def cao(title):
+    # print('窗口标题变成了：', title)
+    window.blockSignals(True)  # 避免出现死循环
+    window.setWindowTitle("muzing " + title)
+    window.blockSignals(False)  # 恢复连接，使下一次还能进行修改
+
+
+window.windowTitleChanged.connect(cao)
+
+window.setWindowTitle("信号的操作案例")
+window.setWindowTitle("修改后的标题")
+window.resize(500, 500)
+window.move(400, 250)
+
+# 2.3展示控件
+window.show()
+
+# 3.应用程序的执行， 进入到消息循环
+sys.exit(app.exec_())
