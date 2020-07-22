50b533ce4562fe579e3406c1f6e024f344bff984
muzing <muzi2001@foxmail.com>
2020-07-22 23:10:37 +0800

    增加QWidget-窗口相关操作。本次commit为临时操作，便于在另一台机器上测试icon图标。

---
+++ 16~XX-QWidget/30-QWidget-窗口相关的操作.py
@@ -0,0 +1,20 @@
+from PyQt5.Qt import *
+import sys
+# 1. 创建一个应用程序对象
+app = QApplication(sys.argv)
+
+# 2.控件的操作
+# 2.1创建控件
+window = QWidget()
+# 2.2设置控件
+
+window.resize(500, 500)
+window.move(400, 250)
+icon = QIcon('Photoshop.ico')
+window.setWindowIcon(icon)
+
+# 2.3展示控件
+window.show()
+
+# 3.应用程序的执行， 进入到消息循环
+sys.exit(app.exec_())
