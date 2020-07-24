dfd5624b9b1a41f9e6a8a219e64945c072b7dcc2
muzing <muzi2001@foxmail.com>
2020-07-24 12:07:26 +0800

    增加QWidget-交互状态-关闭与释放 笔记代码

---
+++ 16~XX-QWidget/36-Qwidget-交互状态-关闭与释放.py
@@ -0,0 +1,28 @@
+from PyQt5.Qt import *
+import sys
+# 1. 创建一个应用程序对象
+app = QApplication(sys.argv)
+
+# 2.控件的操作
+# 2.1创建控件
+window = QWidget()
+# 2.2设置控件
+
+window.setWindowTitle("交互状态-关闭")
+window.resize(500, 500)
+window.move(400, 250)
+
+btn = QPushButton(window)
+btn.setText("按钮")
+btn.destroyed.connect(lambda: print("按钮被释放了"))
+# btn.setVisible(False)
+# btn.setHidden(True)
+# btn.hide()
+# btn.deleteLater()
+btn.setAttribute(Qt.WA_DeleteOnClose, True)  # 关闭时释放对象
+btn.close()
+# 2.3展示控件
+window.show()
+
+# 3.应用程序的执行， 进入到消息循环
+sys.exit(app.exec_())
