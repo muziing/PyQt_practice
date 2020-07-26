e9f0b25dd77a8f29c600c4973e02f711c445af02
muzing <muzi2001@foxmail.com>
2020-07-26 16:43:15 +0800

    增加QPushButton-扁平化 笔记代码

---
+++ 47~XX-QPushButton/49-QPushButton-扁平化.py
@@ -0,0 +1,27 @@
+from PyQt5.Qt import *
+import sys
+# 1. 创建一个应用程序对象
+app = QApplication(sys.argv)
+
+# 2.控件的操作
+# 2.1创建控件
+window = QWidget()
+# 2.2设置控件
+
+window.setWindowTitle("QPUshButton-扁平化")
+window.resize(500, 500)
+window.move(400, 250)
+
+btn = QPushButton("按钮", window)
+btn.move(190, 200)
+btn.setStyleSheet("background-color: cyan")
+
+btn.setFlat(True)
+print("btn是否为扁平化：", btn.isFlat())
+# 设置扁平化之后，按钮的背景也不再绘制
+
+# 2.3展示控件
+window.show()
+
+# 3.应用程序的执行， 进入到消息循环
+sys.exit(app.exec_())
