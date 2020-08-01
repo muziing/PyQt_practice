ca80ddb72317f796d49dc2c8064711f31f46c725
muzing <muzi2001@foxmail.com>
2020-08-01 18:24:04 +0800

    增加 QButtonGroup-创建与使用 笔记代码

---
+++ 61~XX-QButtonGroup/61-QButtonGroup-创建与使用.py
@@ -0,0 +1,37 @@
+from PyQt5.Qt import *
+import sys
+# 1. 创建一个应用程序对象
+app = QApplication(sys.argv)
+
+# 2.控件的操作
+# 2.1创建控件
+window = QWidget()
+# 2.2设置控件
+
+window.setWindowTitle("QButtonGroup的使用")
+window.resize(500, 500)
+window.move(400, 250)
+
+rb_male = QRadioButton("男", window)
+rb_male.move(100, 100)
+rb_female = QRadioButton("女", window)
+rb_female.move(180, 100)
+
+sex_group = QButtonGroup(window)
+sex_group.addButton(rb_male)
+sex_group.addButton(rb_female)
+
+rb_yes = QRadioButton("yes", window)
+rb_yes.move(100, 220)
+rb_no = QRadioButton("no", window)
+rb_no.move(180, 220)
+
+answer_group = QButtonGroup(window)
+answer_group.addButton(rb_yes)
+answer_group.addButton(rb_no)
+
+# 2.3展示控件
+window.show()
+
+# 3.应用程序的执行， 进入到消息循环
+sys.exit(app.exec_())
