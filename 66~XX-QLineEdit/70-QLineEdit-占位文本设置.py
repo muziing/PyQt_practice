32956cc00ad62cdf5cba376696d618b78128239c
muzing <muzi2001@foxmail.com>
2020-08-03 21:25:27 +0800

    增加 QLineEdit-占位文本设置 笔记代码

---
+++ 66~XX-QLineEdit/70-QLineEdit-占位文本设置.py
@@ -0,0 +1,23 @@
+from PyQt5.Qt import *
+import sys
+# 1. 创建一个应用程序对象
+app = QApplication(sys.argv)
+
+# 2.控件的操作
+# 2.1创建控件
+window = QWidget()
+# 2.2设置控件
+
+window.setWindowTitle("QLineEdit-占位文本设置")
+window.resize(500, 500)
+window.move(400, 250)
+
+le = QLineEdit(window)
+le.setPlaceholderText("在这里输入用户名")
+print(le.placeholderText())
+
+# 2.3展示控件
+window.show()
+
+# 3.应用程序的执行， 进入到消息循环
+sys.exit(app.exec_())
