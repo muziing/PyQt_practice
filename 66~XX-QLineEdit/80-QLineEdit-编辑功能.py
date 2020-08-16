9d2005f872eabeb2ca73e9bce7d9e960d63fad9d
muzing <muzi2001@foxmail.com>
2020-08-16 10:02:55 +0800

    增加一些笔记代码

--- 66~XX-QLineEdit/80-QLineEdit-编辑功能.py
+++ 66~XX-QLineEdit/80-QLineEdit-编辑功能.py
@@ -15,6 +15,11 @@ window.move(400, 250)
 
 le = QLineEdit(window)
 le.move(100, 100)
+le.setDragEnabled(True)  # 设置为可以 拖拽
+
+le2 = QLineEdit(window)
+le2.move(100, 150)
+le2.setDragEnabled(False)  # 不能将le2中的文本拖拽到le中
 
 btn = QPushButton("按钮", window)
 btn.move(280, 100)
@@ -24,6 +29,15 @@ def cao():
     # le.backspace()  # 和键盘上 backspace 键有所不同，不能删除选中的多个字符
     # le.del_()
     le.clear()  # 清空 （也可以用setText('')来实现）
+    # le.cursorBackward(True, 3)
+    # # le.copy()  # 先选中一段文本，才能复制
+    # le.cut()  # 剪切
+    # le.setCursorPosition(0)
+    # le.paste()
+    # le.undo()  # 撤销
+    # # print(le.isUndoAvailable())
+    # le.redo()  # 重做
+    # # print(le.isRedoAvailable())
 
 
 btn.clicked.connect(cao)
