8b63e1fddbaf4021ecafcb46fb75f5a127361ad8
muzing <muzi2001@foxmail.com>
2020-09-03 21:22:15 +0800

    增加 QFrame-功能作用 笔记代码

---
+++ 83~84-QFrame/84-QFrame-功能作用.py
@@ -0,0 +1,38 @@
+from PyQt5.Qt import *
+import sys
+# 1. 创建一个应用程序对象
+app = QApplication(sys.argv)
+
+# 2.控件的操作
+# 2.1创建控件
+window = QWidget()
+# 2.2设置控件
+
+window.setWindowTitle("QFrame-功能作用")
+window.resize(500, 500)
+# window.move(400, 250)
+
+frame = QFrame(window)
+frame.resize(200, 200)
+
+# frame.setFrameShape(QFrame.Box)
+# frame.setFrameShape(QFrame.Panel)
+# frame.setFrameShadow(QFrame.Raised)
+
+frame.setFrameStyle(QFrame.Box | QFrame.Raised)
+
+frame.setLineWidth(5)  # 外线宽度
+frame.setMidLineWidth(6)  # 中线宽度
+
+print(frame.frameWidth())
+
+# frame.setFrameRect(QRect(50, 50, 50, 50))  # 框架矩形
+
+frame.move(100, 100)
+
+
+# 2.3展示控件
+window.show()
+
+# 3.应用程序的执行， 进入到消息循环
+sys.exit(app.exec_())
