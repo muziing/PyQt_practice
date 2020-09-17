7206e227248548405b6d40cd59e41ffa57de48bd
muzing <muzi2001@foxmail.com>
2020-09-17 09:10:23 +0800

    增加了 QTextEdit-占位文本设置 笔记代码

---
+++ 86~XX-QTextEdit/87-QTextEdit-占位文本的设置.py
@@ -0,0 +1,29 @@
+from PyQt5.Qt import *
+import sys
+
+
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("QTextEdit-占位文本")
+        self.resize(500, 500)
+        self.move(400, 250)
+        self.setup_ui()
+
+    def setup_ui(self):
+        self.te = QTextEdit(self)
+        self.te.resize(200, 200)
+        self.占位文本的提示()
+
+    def 占位文本的提示(self):
+        self.te.setPlaceholderText("请输入信息")  # 只有在QTextEdit框为空时才显示
+        print(self.te.placeholderText())
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    window = Window()
+    window.show()
+
+    sys.exit(app.exec_())
