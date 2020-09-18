79f3d6d3b73a5b4b655e0b50817c23f1a5e10976
muzing <muzi2001@foxmail.com>
2020-09-18 08:27:45 +0800

    增加 QTextEdit-创建 笔记代码

---
+++ 86~XX-QTextEdit/86-QTextEdit-创建.py
@@ -0,0 +1,26 @@
+from PyQt5.Qt import *
+import sys
+
+
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("QTextEdit的创建")
+        self.resize(500, 500)
+        self.move(400, 250)
+        self.setup_ui()
+
+    def setup_ui(self):
+        te = QTextEdit("QTextEdit中的文字", self)
+        te.move(50, 50)
+        te.resize(300, 300)
+        te.setStyleSheet('background-color: cyan;')
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    window = Window()
+    window.show()
+
+    sys.exit(app.exec_())
