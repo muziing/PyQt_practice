48f249c85d58903b96169a956f38585a7f47fc10
muzing <muzi2001@foxmail.com>
2020-10-01 09:22:24 +0800

    增加 QFormLayout-行操作(2)-设置行 笔记代码

---
+++ 布局管理器/05-QFormLayout-行操作(2).py
@@ -0,0 +1,32 @@
+from PyQt5.Qt import *
+import sys
+
+
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("QFormLayout-行操作")
+        self.resize(500, 500)
+        self.move(400, 250)
+        self.setup_ui()
+
+    def setup_ui(self):
+        name_label = QLabel("姓名：")
+        age_label = QLabel("年龄：")
+        name_le = QLineEdit()
+        age_sb = QSpinBox()
+
+        layout = QFormLayout()
+        self.setLayout(layout)
+
+        layout.setWidget(0, QFormLayout.LabelRole, name_label)  # 设置行，行号、左右位置、设置为
+        layout.setWidget(0, QFormLayout.FieldRole, name_le)  # 这种方法只能设置原来空白的单元格，否则无效
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    window = Window()
+    window.show()
+
+    sys.exit(app.exec_())
