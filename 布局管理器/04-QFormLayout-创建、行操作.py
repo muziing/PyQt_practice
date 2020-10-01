bd61d4002c8c26704f28518a7c68101f879f22b6
muzing <muzi2001@foxmail.com>
2020-10-01 08:56:58 +0800

    更新 QFormLayout-行操作 ，增加插入行、获取行位置信息操作 笔记代码

--- 布局管理器/04-QFormLayout-创建、行操作.py
+++ 布局管理器/04-QFormLayout-创建、行操作.py
@@ -1,6 +1,18 @@
 from PyQt5.Qt import *
 import sys
 
+'''
+    AllNonFixedFieldsGrow = 2
+    DontWrapRows = 0
+    ExpandingFieldsGrow = 1
+    FieldRole = 1
+    FieldsStayAtSizeHint = 0
+    LabelRole = 0
+    SpanningRole = 2
+    WrapAllRows = 2
+    WrapLongRows = 1
+'''
+
 
 class Window(QWidget):
     def __init__(self):
@@ -11,8 +23,8 @@ class Window(QWidget):
         self.setup_ui()
 
     def setup_ui(self):
-        # name_label = QLabel("姓名：(&n):")
-        # age_label = QLabel("年龄：(&g):")
+        name_label = QLabel("姓名：(&n):")
+        age_label = QLabel("年龄：(&g):")
         name_le = QLineEdit()
         age_sb = QSpinBox()
 
@@ -39,10 +51,19 @@ class Window(QWidget):
 
         # layout.addRow(name_label, name_le)
         layout.addRow('姓名（&n）:', name_le)  # 可以替代 name_label.setBuddy(name_le)等操作
-        layout.addRow(sex_label, h_layout)
+        # layout.addRow(sex_label, h_layout)
         # layout.addRow(age_label, age_sb)
         layout.addRow('年龄(&g):', age_sb)
         layout.addRow(submit_button)
+
+        layout.insertRow(2, '性别:', h_layout)  # 插入行
+        # layout.insertRow(10, '性别:', h_layout)  # 索引越界后自动加到最后一行
+
+        print(layout.rowCount())  # 返回总行数
+        print(layout.getWidgetPosition(age_sb))  # 返回一个元组，第一项为行号，第二项为左右位置
+        print(layout.getWidgetPosition(name_le))
+        print(layout.getWidgetPosition(name_label))  # 在布局管理器中找不到此对象时返回元组第一项为 -1
+        print(layout.getLayoutPosition(h_layout))  # 获取子布局管理器位置
 
 
 if __name__ == '__main__':
