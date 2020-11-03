787ea8e3b0af999c55355aad10f97f1bdca7b192
muzing <muzi2001@foxmail.com>
2020-11-03 16:18:48 +0800

    更新 QFormLayout-行操作-移除行 笔记代码

--- 布局管理器/05-QFormLayout-行操作(2).py
+++ 布局管理器/05-QFormLayout-行操作(2).py
@@ -27,8 +27,23 @@ class Window(QWidget):
 
         layout.setWidget(0, QFormLayout.LabelRole, name_label)  # 设置行，行号、左右位置、设置为
         layout.setWidget(0, QFormLayout.FieldRole, name_le)  # 这种方法只能设置原来空白的单元格，否则无效
+        layout.addRow(age_label, age_sb)
         # layout.addRow(name_label)
         # layout.setLayout(0, QFormLayout.LabelRole, h_layout)
+
+        # 移除和删除行
+        age_sb.destroyed.connect(lambda: print("年龄步长被释放"))
+        age_label.destroyed.connect(lambda: print("年龄label被释放"))
+        # layout.removeRow(1)  # 释放
+        # layout.removeRow(age_sb)  # 也可以移除一整行
+        layout.removeWidget(age_label)  # 只是移除了和布局管理器的父子关系
+        age_label.setParent(None)  # 彻底移除了
+        # print(layout.takeRow(1).fieldItem)  # 移除但不释放，返回 QFormLayout.TakeRowResult object
+        # print(layout.takeRow(1).labelItem)  # 移除但不释放，返回 QFormLayout.TakeRowResult object
+        # # 此时会因为一些控件在布局管理器中，一些控件还存在但不在布局管理器中，导致显示混乱，可以通过隐藏后者来解决
+        # age_label.hide()
+        # age_sb.hide()
+        # print(dir(QFormLayout.TakeRowResult))
 
 
 if __name__ == '__main__':
