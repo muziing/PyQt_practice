80a800acba2f9fbef74d65a1f808fca8054e0838
muzing <muzi2001@foxmail.com>
2020-12-24 11:09:53 +0800

    增加 QFormLayout-对齐、间距、字段增长策略 笔记代码

--- 布局管理器/06-QFormLayout-行包装策略、对齐、间距、字段增长策略.py
+++ 布局管理器/06-QFormLayout-行包装策略、对齐、间距、字段增长策略.py
@@ -27,11 +27,28 @@ class Window(QWidget):
         layout.addRow('年龄(&g):', age_sb)
         layout.addRow(submit_button)
 
-        # layout.setRowWrapPolicy(QFormLayout.DontWrapRows)  # 字段总是放在标签旁边（默认设置）
+        # --------行包装策略----------
+        layout.setRowWrapPolicy(QFormLayout.DontWrapRows)  # 字段总是放在标签旁边（默认设置）
         # layout.setRowWrapPolicy(QFormLayout.WrapLongRows)  
         # 标签被赋予足够的水平空间以适合最宽的标签，其余空间被赋予字段
         # 如果字段的最小大小比可用空间宽，则该字段将换行到下一行
-        layout.setRowWrapPolicy(QFormLayout.WrapAllRows)  # 字段总是位于其标签下方
+        # layout.setRowWrapPolicy(QFormLayout.WrapAllRows)  # 字段总是位于其标签下方
+
+        # -----------对齐------------
+        # 整个表单的对齐，默认为左上
+        # layout.setFormAlignment(Qt.AlignLeft | Qt.AlignBottom)  # 下对齐
+        layout.setFormAlignment(Qt.AlignLeft | Qt.AlignVCenter)  # 垂直方向居中
+
+        # 标签的对齐
+        layout.setLabelAlignment(Qt.AlignRight)
+
+        # ----------间距------------
+        layout.setVerticalSpacing(60)
+        layout.setHorizontalSpacing(60)
+
+        # -------字段增长策略--------
+        layout.setFieldGrowthPolicy(QFormLayout.FieldsStayAtSizeHint)  # 这些领域永远不会超过其有效大小的提示
+        layout.setFieldGrowthPolicy(QFormLayout.ExpandingFieldsGrow)  # 水平大小策略为Expanding或MinimumExpanding的字符将增长
 
 
 if __name__ == '__main__':
