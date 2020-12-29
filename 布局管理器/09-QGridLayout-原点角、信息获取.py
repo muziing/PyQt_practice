65079d15fad021f9e8b553f4924a3afd1f715000
muzing <muzi2001@foxmail.com>
2020-12-29 09:56:44 +0800

    增加 QGridLayout-信息获取 笔记代码

---
+++ 布局管理器/09-QGridLayout-原点角、信息获取.py
@@ -0,0 +1,40 @@
+from PyQt5.Qt import *
+import sys
+
+
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("QGridLayout")
+        self.resize(500, 500)
+        self.move(400, 250)
+        self.setup_ui()
+
+    def setup_ui(self):
+        gl = QGridLayout()
+        self.setLayout(gl)
+
+        label1 = QLabel('标签1')
+        label1.setStyleSheet('background-color: red;')
+        label2 = QLabel('标签2')
+        label2.setStyleSheet('background-color: yellow;')
+        label3 = QLabel('标签3')
+        label3.setStyleSheet('background-color: cyan;')
+
+        gl.addWidget(label1, 0, 0)
+        gl.addWidget(label2, 0, 1)
+        gl.addWidget(label3, 1, 0, 1, 2)
+
+        print(gl.rowCount())
+        print(gl.columnCount())
+        print(gl.itemAtPosition(0, 1).widget().text())
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    window = Window()
+    window.show()
+    print(window.layout().cellRect(0, 0))
+
+    sys.exit(app.exec_())
