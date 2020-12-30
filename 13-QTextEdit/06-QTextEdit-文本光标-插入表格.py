25234887470d7670955394ff2b9996ac1310607e
muzing <muzi2001@foxmail.com>
2020-12-30 14:44:55 +0800

    增加 QTextEdit-光标-插入表格 笔记代码

---
+++ 13-QTextEdit/06-QTextEdit-文本光标-插入表格.py
@@ -0,0 +1,39 @@
+from PyQt5.Qt import *
+import sys
+
+
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("QTextEdit-插入表格")
+        self.resize(500, 500)
+        self.move(400, 250)
+        self.setup_ui()
+
+    def setup_ui(self):
+        te = QTextEdit(self)
+        self.te = te
+        te.setText('abc')
+        test_btn = QPushButton("测试按钮", self)
+        test_btn.move(350, 140)
+        test_btn.clicked.connect(self.test_btn_cao)
+
+    def test_btn_cao(self):
+        self.cursor_insert_table()
+
+    def cursor_insert_table(self):
+        tc = self.te.textCursor()  # 创建光标对象
+        ttf = QTextTableFormat()
+        ttf.setAlignment(Qt.AlignRight)  # 右对齐
+        ttf.setCellPadding(6)  # 内边距
+        ttf.setCellSpacing(3)  # 外边距
+        tc.insertTable(5, 3, ttf)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    window = Window()
+    window.show()
+
+    sys.exit(app.exec_())
