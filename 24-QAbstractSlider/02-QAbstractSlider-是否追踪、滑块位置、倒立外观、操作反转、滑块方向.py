67a7cba6ddfc1cad7dbe9c0364b999a8b85513fd
muzing <muzi2001@foxmail.com>
2021-01-04 14:45:53 +0800

    增加 QAbstractSlider-是否追踪、滑块位置 笔记代码

---
+++ 24-QAbstractSlider/02-QAbstractSlider-是否追踪、滑块位置、倒立外观、操作反转、滑块方向.py
@@ -0,0 +1,41 @@
+from PyQt5.Qt import *
+import sys
+
+
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("QABsTractSlider")
+        self.resize(500, 500)
+        self.move(400, 250)
+        self.setup_ui()
+
+    def setup_ui(self):
+        label = QLabel(self)
+        label.setText("0")
+        label.move(300, 100)
+        label.resize(100, 30)
+        sd = QSlider(self)
+        sd.move(100, 100)
+        sd.valueChanged.connect(lambda val: label.setText(str(val)))
+
+        # ------------追踪设置-----------
+        """value是否跟着滑块的位置变化而立即变化，默认值为True。即使设置为False，松开鼠标后也会更新"""
+        print(f'Tracking默认为{sd.hasTracking()}')
+        sd.setTracking(False)  # 手动设置为不追踪
+
+        # -----------滑块位置-----------
+        sd.setSliderPosition(88)  # 在非追踪状态下，会出现滑块位置在88但值为0的情况
+
+        # ----------倒立外观----------
+        
+
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    window = Window()
+    window.show()
+
+    sys.exit(app.exec_())
