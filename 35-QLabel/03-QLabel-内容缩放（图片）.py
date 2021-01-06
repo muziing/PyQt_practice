0e261b446140ec71f434b94059b75d711db6c4d9
muzing <muzi2001@foxmail.com>
2021-01-06 10:20:10 +0800

    增加 QLabel-内容缩放 笔记代码

---
+++ 35-QLabel/03-QLabel-内容缩放（图片）.py
@@ -0,0 +1,27 @@
+from PyQt5.Qt import *
+import sys
+
+
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("QLabel-内容缩放")
+        self.resize(500, 500)
+        self.move(400, 250)
+        self.setup_ui()
+
+    def setup_ui(self):
+        label = QLabel(self)
+        label.move(100, 100)
+        label.resize(80, 80)
+        label.setPixmap(QPixmap('../Icons/OS_Ubuntu_128px.ico'))
+        label.setScaledContents(True)  # 设置内容缩放
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    window = Window()
+    window.show()
+
+    sys.exit(app.exec_())
