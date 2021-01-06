fba5aa86ced4401e323233a2341f57887b8f5021
muzing <muzi2001@foxmail.com>
2021-01-06 13:29:01 +0800

    增加 QLabel-信号 笔记代码

---
+++ 35-QLabel/07-QLabel-信号.py
@@ -0,0 +1,31 @@
+from PyQt5.Qt import *
+import sys
+
+
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("QLabel-信号")
+        self.resize(500, 500)
+        self.move(400, 250)
+        self.setup_ui()
+
+    def setup_ui(self):
+        label = QLabel(self)
+        label.move(100, 100)
+        label.setText("muzing的个人博客链接为 <a href=https://muzing.top>https://muzing.top</a>")
+        label.setTextFormat(Qt.RichText)
+        # label.setOpenExternalLinks(True)
+        label.setOpenExternalLinks(False)
+
+        # label.linkHovered.connect(lambda val: print("鼠标指向了链接", val))
+        label.linkActivated.connect(lambda val: print("鼠标点击了链接", val))  # 仅在 OpenExternalLinks(False)时有效
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    window = Window()
+    window.show()
+
+    sys.exit(app.exec_())
