4c19103f5dbdb33563ccd5b9ef21684d7bdb1bf0
muzing <muzi2001@foxmail.com>
2021-01-07 11:58:27 +0800

    增加 QErrorMessage-创建、功能作用、Debug 笔记代码

---
+++ 38-QErrorMessage/01-QErrorMessage-创建、功能作用.py
@@ -0,0 +1,31 @@
+from PyQt5.Qt import *
+import sys
+
+
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("QErrorMessage")
+        self.resize(500, 500)
+        self.move(400, 250)
+        self.setup_ui()
+
+    def setup_ui(self):
+        em = QErrorMessage(self)
+        em.setWindowTitle("错误提示")
+
+        em.showMessage("在此处展示一些错误提示信息")  # 设置内容文本，调用这个方法还会使窗口自动弹出，非模态
+        em.showMessage("在此处展示一些错误提示信息")
+        em.showMessage("在此处展示一些错误提示信息")  # 取消勾选后可以跳过提示信息完全相同的其他提示框
+        em.showMessage("另外的一些错误提示信息")  # 提示信息不同的其他提示框依然会显示
+
+        # em.exec()  # 应用程序级的模态
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    window = Window()
+    window.show()
+
+    sys.exit(app.exec_())
---
+++ 38-QErrorMessage/02-QErrorMessage-Debug.py
@@ -0,0 +1,25 @@
+from PyQt5.Qt import *
+import sys
+
+
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("QErrorMessage-Debug")
+        self.resize(500, 500)
+        # self.move(400, 250)
+        self.setup_ui()
+
+    def setup_ui(self):
+        QErrorMessage.qtHandler()
+        # qDebug("XXX")
+        qWarning("123")
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    window = Window()
+    # window.show()
+
+    sys.exit(app.exec_())
