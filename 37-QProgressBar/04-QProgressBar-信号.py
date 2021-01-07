fce90156f2c9286e786f6161216fafc5dfea6f3e
muzing <muzi2001@foxmail.com>
2021-01-07 11:27:49 +0800

    增加 QProgressBar-信号 笔记代码

---
+++ 37-QProgressBar/04-QProgressBar-信号.py
@@ -0,0 +1,35 @@
+from PyQt5.Qt import *
+import sys
+
+
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("QProgressBar")
+        self.resize(500, 500)
+        self.move(400, 250)
+        self.setup_ui()
+
+    def setup_ui(self):
+        pb = QProgressBar(self)
+        pb.move(100, 100)
+
+        timer = QTimer(pb)
+
+        def change_progress():
+            if pb.value() == pb.maximum():
+                timer.stop()
+            pb.setValue(pb.value() + 1)
+        timer.timeout.connect(change_progress)
+        timer.start(500)
+
+        pb.valueChanged.connect(lambda val: print(f"当前进度值为{val}"))
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    window = Window()
+    window.show()
+
+    sys.exit(app.exec_())
