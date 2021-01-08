84583859b40275c4381def53200de8b7a3748d31
muzing <muzi2001@foxmail.com>
2021-01-08 20:04:00 +0800

    增加 QAbstractSlider-倒立外观、滑块方向 笔记代码

--- 24-QAbstractSlider/02-QAbstractSlider-是否追踪、滑块位置、倒立外观、操作反转、滑块方向.py
+++ 24-QAbstractSlider/02-QAbstractSlider-是否追踪、滑块位置、倒立外观、操作反转、滑块方向.py
@@ -22,14 +22,17 @@ class Window(QWidget):
         # ------------追踪设置-----------
         """value是否跟着滑块的位置变化而立即变化，默认值为True。即使设置为False，松开鼠标后也会更新"""
         print(f'Tracking默认为{sd.hasTracking()}')
-        sd.setTracking(False)  # 手动设置为不追踪
+        # sd.setTracking(False)  # 手动设置为不追踪
 
         # -----------滑块位置-----------
         sd.setSliderPosition(88)  # 在非追踪状态下，会出现滑块位置在88但值为0的情况
 
         # ----------倒立外观----------
-        
+        # sd.setInvertedAppearance(True)  # 设置倒立外观，键盘方向键上下控制数值增减而不是滑块上下
+        # sd.setInvertedControls(True)  # 设置键盘方向键上下控制的反转
 
+        # --------滑块方向---------
+        sd.setOrientation(Qt.Horizontal)  # 滑块方向设为水平
 
 
 if __name__ == '__main__':
