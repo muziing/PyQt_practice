1ca9534979dee69ebba931abfb6f37678b914770
muzing <muzi2001@foxmail.com>
2021-01-09 09:15:38 +0800

    增加 QRubberBand-创建 笔记代码

---
+++ 28-QRubberBand/01-QRubberBand-创建.py
@@ -0,0 +1,27 @@
+from PyQt5.Qt import *
+import sys
+
+
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("QRubberBand")
+        self.resize(500, 500)
+        self.move(400, 250)
+        self.setup_ui()
+
+    def setup_ui(self):
+        # rb = QRubberBand(QRubberBand.Line, self)
+        rb = QRubberBand(QRubberBand.Rectangle, self)
+        rb.setGeometry(10, 10, 60, 60)
+        print(rb.isVisible())
+        rb.show()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    window = Window()
+    window.show()
+
+    sys.exit(app.exec_())
