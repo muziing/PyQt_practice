46871b54c415a5e619e006dc73f3fae885846797
muzing <muzi2001@foxmail.com>
2021-01-12 17:54:22 +0800

    增加 QFiledialog-静态方法-弹出文件操作对话框

---
+++ 32-QFileDialog/01-QFileDialog-静态方法-弹出文件操作对话框.py
@@ -0,0 +1,33 @@
+from PyQt5.Qt import *
+import sys
+
+
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("QFileDialog")
+        self.resize(500, 500)
+        self.move(400, 250)
+        self.setup_ui()
+
+    def setup_ui(self):
+        # result = QFileDialog.getOpenFileName(self, "选择一个py文件", "../",
+        #                                      "ALL(*, *);;Images(*.png *.jpg);;Python文件(*.py)",
+        #                                      "Python文件(*.py)")
+        # result = QFileDialog.getOpenFileNames(self, "选择一个py文件", "../",
+        #                                       "ALL(*, *);;Images(*.png *.jpg);;Python文件(*.py)",
+        #                                       "Python文件(*.py)")
+        # result = QFileDialog.getOpenFileUrl(self, '选择一个py文件', '../', '所有文件(*.*);;Python文件(*.py);;图片(*.png *.jpg)')
+        result = QFileDialog.getSaveFileName(self, "保存为py文件", "../",
+                                             "ALL(*, *);;Images(*.png *.jpg);;Python文件(*.py)",
+                                             "Python文件(*.py)")
+        print(result)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    window = Window()
+    window.show()
+
+    sys.exit(app.exec_())
