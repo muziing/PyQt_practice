369b7f22c268bf2fc23b3a04b31228aae173b94a
muzing <muzi2001@foxmail.com>
2021-01-13 16:42:56 +0800

    增加 QFileDialog-静态方法-弹出文件夹操作对话框 笔记代码

---
+++ 32-QFileDialog/02-QFileDialog-静态方法-弹出文件夹操作对话框.py
@@ -0,0 +1,25 @@
+from PyQt5.Qt import *
+import sys
+
+
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("QFiledialog-静态方法")
+        self.resize(500, 500)
+        self.move(400, 250)
+        self.setup_ui()
+
+    def setup_ui(self):
+        # result = QFileDialog.getExistingDirectory(self, "选择一个文件夹", "../")  # 返回值为文件夹的绝对路径
+        result = QFileDialog.getExistingDirectoryUrl(self, "选择一个文件夹", QUrl("../"))  # 返回值为QUrl
+        print(result)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    window = Window()
+    window.show()
+
+    sys.exit(app.exec_())
