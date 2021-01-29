afdae8d19c55cf80c672eabfb5a03b81e6aeaf06
muzing <muzi2001@foxmail.com>
2021-01-29 12:02:19 +0800

    为README增加toc目录；增加自动生成项目toc目录的脚本

---
+++ toc_script.py
@@ -0,0 +1,18 @@
+import os
+"""用于生成整个PyQt_practice项目的toc目录，markdown格式"""
+
+abs_addr = os.getcwd()
+with open('./toc.md', 'wt', encoding='UTF-8') as f:
+    lines = list()
+    dir_list = list()
+    for i in os.listdir('./'):
+        if i[0].isdigit() and i[1].isdigit():
+            dir_list.append(i)
+    for n in dir_list:
+        lines.append(f'### {n}\n')
+        os.chdir(f'{abs_addr}/{n}')
+        file_names = os.listdir('./')
+        for m in file_names:
+            lines.append(f'[{m}](./{n}/{m})\n')
+    f.writelines(lines)
+
