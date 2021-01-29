d6f07a04b135fefdc6880074a7dc53f17a4175f7
muzing <muzi2001@foxmail.com>
2021-01-29 10:08:20 +0800

    增加 QColorDialog-创建 笔记代码

---
+++ 31-QColorDialog/01-QColorDialog-创建.py
@@ -0,0 +1,25 @@
+from PyQt5.Qt import *
+import sys
+
+
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("QColorDialog")
+        self.resize(500, 500)
+        self.move(400, 250)
+        self.setup_ui()
+
+    def setup_ui(self):
+        cd = QColorDialog(QColor(20, 154, 151), self)  # 可以传入创建时的默认颜色
+        cd.setWindowTitle('选择颜色')
+        cd.show()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    window = Window()
+    window.show()
+
+    sys.exit(app.exec_())
