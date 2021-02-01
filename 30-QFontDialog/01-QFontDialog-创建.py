f97f3813e42ac6adeab5d145d047eb68a7da66bc
muzing <muzi2001@foxmail.com>
2021-02-01 20:10:34 +0800

    增加 QFontDialog-创建 笔记代码

---
+++ 30-QFontDialog/01-QFontDialog-创建.py
@@ -0,0 +1,30 @@
+from PyQt5.Qt import *
+import sys
+
+
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("QFontDialog")
+        self.resize(500, 500)
+        self.move(400, 250)
+        self.setup_ui()
+
+    def setup_ui(self):
+        btn = QPushButton('测试按钮', self)
+        btn.move(100, 100)
+        btn.clicked.connect(lambda: fd.open())
+
+        font = QFont()
+        font.setFamily("宋体")
+        font.setPointSize(22)
+        fd = QFontDialog(font, self)  # 可以在创建时传入一个QFont对象作为默认字体
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    window = Window()
+    window.show()
+
+    sys.exit(app.exec_())
