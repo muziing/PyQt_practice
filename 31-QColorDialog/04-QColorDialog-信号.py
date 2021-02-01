6597a77fbdc6790d68705448c7e5980f0ea09500
muzing <muzi2001@foxmail.com>
2021-02-01 19:58:04 +0800

    增加 QColorDialog-信号 笔记代码

---
+++ 31-QColorDialog/04-QColorDialog-信号.py
@@ -0,0 +1,39 @@
+from PyQt5.Qt import *
+import sys
+
+
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("QColorDialog-信号")
+        self.resize(500, 500)
+        self.move(400, 250)
+        self.setup_ui()
+
+    def setup_ui(self):
+        btn = QPushButton('测试按钮', self)
+        btn.move(100, 100)
+
+        def test():
+            cd = QColorDialog(self)
+
+            def sel_color(color):
+                palette = QPalette()
+                palette.setColor(QPalette.ButtonText, color)
+                self.setPalette(palette)
+
+            # cd.colorSelected.connect(sel_color)
+            cd.currentColorChanged.connect(sel_color)
+
+            cd.show()
+
+        btn.clicked.connect(test)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    window = Window()
+    window.show()
+
+    sys.exit(app.exec_())
