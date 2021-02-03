a6f6b063368b85819cbe43d9df8d924e223c894f
muzing <muzi2001@foxmail.com>
2021-02-03 18:46:26 +0800

    增加 QInputDialog-静态方法 笔记代码

---
+++ 33-QInputDialog/01-QInputDialog-静态方法.py
@@ -0,0 +1,28 @@
+from PyQt5.Qt import *
+import sys
+
+
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("QInputDialog")
+        self.resize(500, 500)
+        self.move(400, 250)
+        self.setup_ui()
+
+    def setup_ui(self):
+        # result = QInputDialog.getInt(self, "窗口标题", "请输入一个整数", 666, step=10)
+        # result = QInputDialog.getDouble(self, "窗口标题", "请输入一个小数", 66.6, decimals=3)
+        # result = QInputDialog.getText(self, "窗口标题", "提示标签", echo=QLineEdit.Password)
+        # result = QInputDialog.getMultiLineText(self, "窗口标题", "输入多行文本", "default")
+        result = QInputDialog.getItem(self, "窗口标题", "选择一项", ["1", "2", "3"], 2, True)  # 默认选择第三项，可以编辑
+        print(result)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    window = Window()
+    window.show()
+
+    sys.exit(app.exec_())
