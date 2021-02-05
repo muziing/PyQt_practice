a7b18a90406ab1e514139018aa456d1b03c12c9e
muzing <muzi2001@foxmail.com>
2021-02-05 23:14:10 +0800

    增加 QSpinBox-值的设置和获取、自定义展示格式 笔记代码

---
+++ 17-QSpinBox/03-QSpinBox-值的设置和获取、自定义展示格式.py
@@ -0,0 +1,37 @@
+from PyQt5.Qt import *
+import sys
+
+
+# ------自定义展示格式--------
+class MySpinBox(QSpinBox):
+    def textFromValue(self, v: int) -> str:
+        # 修改的只是展示效果，真实数值没有变
+        print(v)
+        return str(v) + "*" + str(v)
+
+
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("QSpinBox")
+        self.resize(500, 500)
+        self.move(400, 250)
+        self.setup_ui()
+
+    def setup_ui(self):
+        sb = MySpinBox(self)
+        sb.resize(100, 25)
+        sb.move(100, 100)
+
+        # ------值的设置与获取-------
+        sb.setValue(20)  # 如果设置的数值超过范围，则自动取范围边界值
+        sb.value()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    window = Window()
+    window.show()
+
+    sys.exit(app.exec_())
