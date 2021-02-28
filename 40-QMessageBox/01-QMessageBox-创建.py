ea0bcc6b1668c7ecf2f5743994ba6ddc42b91cd7
muzing <muzi2001@foxmail.com>
2021-02-28 10:05:35 +0800

    增加一些 QMessageBox 设置选项 笔记代码

--- 40-QMessageBox/01-QMessageBox-创建.py
+++ 40-QMessageBox/01-QMessageBox-创建.py
@@ -18,7 +18,7 @@ QMessageBox.YesToAll 使用YesRole定义的“Yes to All” 按钮
 
 """
 
-"""QMessageBox.Icon
+""" QMessageBox.Icon
 QMessageBox.NoIcon
 QMessageBox.Question
 QMessageBox.Information
@@ -44,12 +44,28 @@ class Window(QWidget):
         # mb.show()  # 一定为模态，即使使用show()方法也仍为模态
 
         mb.setWindowTitle("消息提示")
+
+        # 设置图标
         # mb.setIcon(QMessageBox.Information)  # 设置标准图标
         mb.setIconPixmap(QPixmap('../Icons/python_96px.ico').scaled(40, 40))  # 设置自定义图标
+
+        # 设置主标题
         mb.setText("<h3>文件内容已经被修改</h3>")  # 设置主标题
+        # mb.setTextFormat(Qt.PlainText)  # 设置主标题文本格式
+        # mb.setTextFormat(Qt.RichText)
+        mb.setTextFormat(Qt.AutoText)
+
+        # 设置提示文本（副标题）
         mb.setInformativeText("是否直接关闭不保存")  # 设置副标题
-        mb.setCheckBox(QCheckBox("下次不再提醒", mb))  # 设置复选框
+        # print(mb.informativeText())
+
+        # 设置详细文本
         mb.setDetailedText("你修改的内容是……我怎么知道你改了点啥")  # 设置详情（不支持富文本）
+        # print(mb.detailedText())
+
+        # 设置复选框
+        mb.setCheckBox(QCheckBox("下次不再提醒", mb))  # 设置复选框
+        mb.checkBox().toggled.connect(lambda: print('复选框被点击了'))
 
         mb.open()
 
