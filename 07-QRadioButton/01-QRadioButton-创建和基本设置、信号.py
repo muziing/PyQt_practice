d96a5ca9baa03c41a02dc24890c6620a332d1ad4
muzing <muzi2001@foxmail.com>
2021-04-30 18:14:48 +0800

    调整了一些注释

--- 07-QRadioButton/01-QRadioButton-创建和基本设置、信号.py
+++ 07-QRadioButton/01-QRadioButton-创建和基本设置、信号.py
@@ -1,5 +1,6 @@
 from PyQt5.Qt import *
 import sys
+
 # 1. 创建一个应用程序对象
 app = QApplication(sys.argv)
 
@@ -12,18 +13,20 @@ window.setWindowTitle("QRadioButton的创建")
 window.resize(500, 500)
 window.move(400, 250)
 
-rb_male = QRadioButton("男-&Male", window)
+rb_male = QRadioButton("男-&Male", window)  # 因为在字母M之前使用了&符号，自动添加一个Alt+M的快捷键
 rb_male.setIcon(QIcon('../Icons/man_96px.ico'))
-rb_male.setShortcut("Alt+M")
+rb_male.setShortcut("Alt+M")  # 设置快捷键
 rb_male.move(100, 100)
-rb_male.setChecked(True)
+rb_male.setChecked(True)  # 手动设置已为被选中
 
 rb_female = QRadioButton("女-&Female", window)
 rb_female.setIcon(QIcon('../Icons/woman_96px.ico'))
 rb_female.move(100, 150)
-rb_female.toggled.connect(lambda isChecked: print(isChecked))  # 选中状态发生改变时
+rb_female.toggled.connect(lambda isChecked: print("rb_female选中状态变成了：", isChecked))  # 选中状态发生改变时执行print槽函数
 rb_female.setAutoExclusive(False)  # 取消了独占性
-"""独占性：在同一父控件下，且独占性为True的控件之间才存在"""
+
+# 独占性：在同一父控件下，且独占性为True的控件之间才存在
+
 # 2.3展示控件
 window.show()
 
--- 07-QRadioButton/02-QRadioButton-多组互斥问题.py
+++ 07-QRadioButton/02-QRadioButton-多组互斥问题.py
@@ -1,12 +1,9 @@
 from PyQt5.Qt import *
 import sys
-# 1. 创建一个应用程序对象
+
 app = QApplication(sys.argv)
 
-# 2.控件的操作
-# 2.1创建控件
 window = QWidget()
-# 2.2设置控件
 
 window.setWindowTitle("QRadioButton-多组互斥问题")
 window.resize(500, 500)
@@ -20,9 +17,10 @@ red.move(50, 50)
 green = QWidget(window)
 green.resize(200, 200)
 green.setStyleSheet("background-color: green;")
-green.move(red.x()+red.width(), red.y()+red.height())
+green.move(red.x() + red.width(), red.y() + red.height())
 
-"""处理多组互斥——糟糕的解决方案：创建多个父控件，同组的放在同一个父控件中"""
+# 处理多组互斥——糟糕的解决方案：创建多个父控件，同组的放在同一个父控件中
+# 好的解决方案：QButtonGroup
 
 rb_male = QRadioButton("男", red)
 rb_male.move(10, 10)
@@ -34,8 +32,6 @@ rb_yes.move(10, 20)
 rb_no = QRadioButton("no", green)
 rb_no.move(60, 20)
 
-# 2.3展示控件
 window.show()
 
-# 3.应用程序的执行， 进入到消息循环
 sys.exit(app.exec_())
