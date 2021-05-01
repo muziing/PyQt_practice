b931dcb7753634a967d1cd562a31f984fe3a0305
muzing <muzi2001@foxmail.com>
2021-05-01 08:57:23 +0800

    增加一个Demo: QDockWidget

---
+++ Demos/QDockWidget.py
@@ -0,0 +1,32 @@
+from PyQt5.Qt import *
+import sys
+
+"""
+停靠控件，可以把小窗拖入并停靠到主窗口上
+原作者: https://blog.csdn.net/qq_40243295
+"""
+
+
+class DockDemo(QMainWindow):
+    def __init__(self, parent=None):
+        """停靠部件QDockWidget"""
+        super(DockDemo, self).__init__(parent)
+        self.setWindowTitle("停靠控件：QDockWidget")
+
+        self.items = QDockWidget("Dockable", self)
+        self.listWidget = QListWidget()
+        self.listWidget.addItem("item1")
+        self.listWidget.addItem("item2")
+        self.listWidget.addItem("item3")
+
+        self.items.setWidget(self.listWidget)
+        self.setCentralWidget(QLineEdit())
+        self.items.setFloating(True)
+        self.addDockWidget(Qt.RightDockWidgetArea, self.items)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    main = DockDemo()
+    main.show()
+    sys.exit(app.exec_())
