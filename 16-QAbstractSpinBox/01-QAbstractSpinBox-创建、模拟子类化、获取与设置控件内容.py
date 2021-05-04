cebdd78ed94b56a152c4313621efeb67ec34f38b
muzing <muzi2001@foxmail.com>
2021-05-04 10:42:22 +0800

    Fix a bug， add some comments

--- 16-QAbstractSpinBox/01-QAbstractSpinBox-创建、模拟子类化、获取与设置控件内容.py
+++ 16-QAbstractSpinBox/01-QAbstractSpinBox-创建、模拟子类化、获取与设置控件内容.py
@@ -3,16 +3,17 @@ import sys
 
 
 class MyASB(QAbstractSpinBox):
+    """自定义的子类，实现限制数值范围在0~9的功能"""
     def __init__(self, parent, num: int = 0):
         super().__init__(parent)
         self.current_num = 0
         self.lineEdit().setText(f'{num}')
 
     def stepEnabled(self) -> 'QAbstractSpinBox.StepEnabled':
-        # 目标：限制在0-9范围内
+        # 目标：限制用户通过箭头输入数值在0-9范围内
         try:
             self.current_num = int(self.text())
-        except TypeError:
+        except ValueError:
             self.current_num = 0
         if self.current_num == 0:
             return QAbstractSpinBox.StepUpEnabled
@@ -26,6 +27,8 @@ class MyASB(QAbstractSpinBox):
     def stepBy(self, steps: int) -> None:
         self.current_num = self.current_num + steps
         self.lineEdit().setText(str(self.current_num))
+        # 仅限于用户通过键盘方向键或者鼠标点击上下箭头改变数值
+        # 此时用户还可以通过键盘数字键输入0~9之外的数值
 
 
 class Window(QWidget):
@@ -49,6 +52,7 @@ class Window(QWidget):
     def btn_test(self):
         print(self.asb.text())  # 获取文本
         self.asb.lineEdit().setText('888')  # 设置文本
+        # 不受MyASB的0~9限制，仍可设置为888
 
 
 if __name__ == '__main__':
