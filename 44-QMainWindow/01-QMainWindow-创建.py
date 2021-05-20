156ca2e96923d9fbde2b79bea685d6daf0f3994d
muzing <muzi2001@foxmail.com>
2021-05-20 20:03:34 +0800

    Update QMainWindow-创建

--- 44-QMainWindow/01-QMainWindow-创建.py
+++ 44-QMainWindow/01-QMainWindow-创建.py
@@ -5,30 +5,43 @@ import sys
 class Window(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.td = QTextEdit()
+
+        self.text_edit = QTextEdit(self)
+        self.status_bar = QStatusBar(self)
+        self.menu_bar = QMenuBar(self)
+        self.toolbar = QToolBar(self)
+
         self.setWindowTitle("QMainWindow")
         self.resize(500, 500)
         self.move(400, 250)
         self.setup_ui()
 
     def open_file(self):
-        result = QFileDialog.getOpenFileName(self, "选择一个py文件", "../",
-                                             "ALL(*, *);;Images(*.png *.jpg);;Python文件(*.py)",
-                                             "Python文件(*.py)")
-        with open(result[0], 'r', encoding="UTF-8") as f:
-            self.td.setText(f.read())
+        file_name = QFileDialog.getOpenFileName(self, "选择一个py文件", "../",
+                                                "ALL(*, *);;Images(*.png *.jpg);;Python文件(*.py)",
+                                                "Python文件(*.py)")[0]
+        if file_name:
+            with open(file_name, 'r', encoding="UTF-8") as f:
+                self.text_edit.setText(f.read())
+            self.status_bar.showMessage(f"成功打开了文件{file_name}")
 
     def setup_ui(self):
-        self.setCentralWidget(self.td)
+        # -------中央控件-------
+        self.setCentralWidget(self.text_edit)
 
-        toolbar = QToolBar()
-        toolbar.addAction("打开文件", self.open_file)
-        toolbar.addAction("关闭", lambda: self.close())
-        self.addToolBar(toolbar)
+        # -------菜单栏--------
+        self.menu_bar.addAction("打开文件", self.open_file)
+        self.setMenuBar(self.menu_bar)
 
-        sb = QStatusBar()
-        sb.showMessage("一条状态栏消息")
-        self.setStatusBar(sb)
+        # ------工具栏--------
+        self.toolbar.addAction("关闭窗口", lambda: self.close())
+        self.toolbar.addAction(QIcon('../Icons/text-icons/undo_96px.ico'), "撤销", self.text_edit.undo)
+        self.toolbar.addAction(QIcon('../Icons/text-icons/redo_96px.ico'), "重做", self.text_edit.redo)
+        self.addToolBar(self.toolbar)
+
+        # -----状态栏----------
+        self.status_bar.showMessage("一条状态栏消息")
+        self.setStatusBar(self.status_bar)
 
 
 if __name__ == '__main__':
