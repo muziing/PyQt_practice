61db01d558056fc33a78fb2606812e63af26a6d1
muzing <muzi2001@foxmail.com>
2022-07-16 00:18:22 +0800

    Update 43-02
    信号与槽-重载、多个参数

--- 43-pyqtSignal/02-pyqtSignal-重载、多个参数.py
+++ 43-pyqtSignal/02-pyqtSignal-重载、多个参数.py
@@ -4,8 +4,11 @@ from PyQt5.Qt import *
 
 
 class Btn(QPushButton):
+    """自定义按钮类，重写事件以实现自定义信号"""
+
+    # 自定义信号
     rightClicked = pyqtSignal([str], [int])  # 每次只发送一个str或int类型的数据，应用于重载等情况
-    leftClicked = pyqtSignal([str], [int, str])  # 多个参数
+    leftClicked = pyqtSignal([str], [int, str])  # 可以传出多个参数
 
     def mousePressEvent(self, e) -> None:
         super().mousePressEvent(e)
@@ -23,24 +26,24 @@ class Window(QWidget):
         super().__init__()
         self.setWindowTitle("信号")
         self.resize(500, 500)
-        self.move(400, 250)
         self.setup_ui()
 
     def setup_ui(self):
         btn = Btn("测试按钮", self)
         btn.move(100, 100)
 
-        btn.rightClicked.connect(
-            lambda content: print("按钮被鼠标右键点击击了", content)
-        )  # 没有指明需要的参数类型，默认取第一种
-        # btn.rightClicked[int].connect(lambda content: print('按钮被鼠标右键点击击了', content))  # 指明需要的参数类型，找int那一项
-        btn.leftClicked[int, str].connect(lambda c1, c2: print("鼠标中键被点击了", c1, c2))
+        # 没有指明需要的参数类型，默认取第一种
+        btn.rightClicked.connect(lambda content: print("按钮被鼠标右键点击击了", content))
+
+        # 指明需要的参数类型，找int那一项
+        # btn.rightClicked[int].connect(lambda content: print('按钮被鼠标右键点击击了', content))
+
+        # 传递多个参数
+        btn.leftClicked[int, str].connect(lambda c1, c2: print("鼠标左键被点击了", c1, c2))
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-
     window = Window()
     window.show()
-
     sys.exit(app.exec_())
