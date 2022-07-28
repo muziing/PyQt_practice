c7a3175416b0c6f11e2ea6b1bffc6100a38cbd8e
muzing <muzi2001@foxmail.com>
2022-07-28 09:00:33 +0800

    Update 10-10

--- 10-QLineEdit/10-QLineEdit-验证器-掩码.py
+++ 10-QLineEdit/10-QLineEdit-验证器-掩码.py
@@ -3,14 +3,18 @@ import sys
 from PyQt5.Qt import *
 
 """
-要求：
+掩码是另一种更为严格的输入验证方式，用户输入的每一位都必须严格
+符合掩码设置的要求
+
+掩码详情见：https://doc.qt.io/qt-5/qlineedit.html#inputMask-prop
+
+案例：
 总共输入5位  左边2（必须是大写字母） - 右边2（必须是数字）
 """
 
 app = QApplication(sys.argv)
 
 window = QWidget()
-
 window.setWindowTitle("QLineEdit-验证器-掩码")
 window.resize(500, 500)
 window.move(400, 250)
